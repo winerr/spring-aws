ea10ab96e4406f39bbaed2529fa068047681531f
Dmytro Vertepnyi <Dmytro_Vertepnyi@epam.com>
2021-09-27 14:15:52 +0300

    initial commit

--- src/main/java/org/aws/study/aws/controller/HelloController.java
+++ src/main/java/org/aws/study/aws/controller/HelloController.java
@@ -1,11 +1,13 @@
 package org.aws.study.aws.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
 
-@RestController("/hello")
+@RestController
+@RequestMapping("/hello")
 public class HelloController {
 
     @GetMapping
